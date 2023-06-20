36cdfbecaeac2fd5d152285d96da4a21a45fbbca
Gio d'Amelio <giodamelio@gmail.com>
2023-06-20 11:32:30 -0700

    Keep state in S3

--- versions.tf
+++ versions.tf
@@ -1,4 +1,10 @@
 terraform {
+  backend "s3" {
+    bucket = "terraform-state-gio-ninja"
+    key    = "state"
+    region = "us-west-2"
+  }
+
   required_providers {
     tailscale = {
       source  = "tailscale/tailscale"
