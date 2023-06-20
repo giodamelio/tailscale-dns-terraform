2f38ac69c7e4884ac664003dfe202a48ffaa9e3e
Gio d'Amelio <giodamelio@gmail.com>
2023-06-20 14:18:19 -0700

    Super simple golang http handler

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module tailscale-dns-terraform
 
 go 1.20
+
+require (
+	github.com/aws/aws-lambda-go v1.41.0
+	github.com/awslabs/aws-lambda-go-api-proxy v0.14.0
+)
--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	"log"
+	"io"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
 )
 
 func main() {
-	log.Println("Hello World!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "Hello World")
+	})
+
+	lambda.Start(httpadapter.New(http.DefaultServeMux).ProxyWithContext)
 }
